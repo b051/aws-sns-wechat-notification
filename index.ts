20040637daa341424110619d7a55bed71497f1c3
Rex Sheng <shengning@gmail.com>
2018-12-15 16:08:27 +0800

    fix parser

--- index.ts
+++ index.ts
@@ -8,14 +8,22 @@ const port = process.env.PORT || 3030
 app.use(async ctx => {
   const messageType = ctx.header['x-amz-sns-message-type']
   if (messageType === 'SubscriptionConfirmation') {
-    const body = parse.json(ctx)
+    const body = await parse.json(ctx)
     console.log('SubscriptionConfirmation body', JSON.stringify(body))
-    const res = await request.get(body['SubscribeURL'])
-    console.log(res.status, res.body)
+    if (body['SubscribeURL']) {
+      const res = await request.get(body['SubscribeURL'])
+      console.log(res.status, res.body)
+      ctx.body = {}
+    } else {
+      ctx.status = 404
+    }
   } else if (messageType === 'Notification') {
     const body = parse.json(ctx)
     console.log('Subject:', body['Subject'])
     console.log('Message:', body['Message'])
+    ctx.body = {}
+  } else {
+    ctx.status = 404
   }
 })
 app.listen(port, () => {
